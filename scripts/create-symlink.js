f6fde09bc3275c06c10e2c0b2ac69101aac44bac
SergioCifuentes <sergiocifuentes1485@gmail.com>
2024-09-12 17:43:55 -0600

    build fix

--- scripts/create-symlink.js
+++ scripts/create-symlink.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const srcPath = path.resolve(__dirname, '../src/node_modules');
-const nodeModulesPath = path.resolve(__dirname, '../node_modules');
+const nodeModulesPath = path.resolve(__dirname, '..', 'src', 'node_modules');
+const workspaceNodeModulesPath = path.resolve(__dirname, '..', 'node_modules');
 
-if (!fs.existsSync(srcPath)) {
-  fs.symlinkSync(nodeModulesPath, srcPath, 'dir');
+// Check if the symlink exists
+if (!fs.existsSync(nodeModulesPath)) {
+  // Create symlink if it doesn't exist
+  try {
+    fs.symlinkSync(workspaceNodeModulesPath, nodeModulesPath, 'junction'); // 'junction' works for Windows too
+    console.log('Symlink created successfully');
+  } catch (err) {
+    console.error('Error creating symlink:', err);
+  }
+} else {
+  console.log('Symlink already exists');
 }
