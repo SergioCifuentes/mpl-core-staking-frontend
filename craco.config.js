0a5473fb3334b79a9157a5e03f8e9ca4733a05e6
SergioCifuentes <sergiocifuentes1485@gmail.com>
2024-09-12 17:36:50 -0600

    build fix

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,26 @@
-// craco.config.js
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const path = require('path');
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules'];
+      
+      // Remove symlinks from fallback
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
-        symlinks: true,
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
         buffer: require.resolve('buffer/'),
         process: require.resolve('process/browser'),
-        // Add other fallbacks as needed
       };
+
+      // Add symlinks option under resolve, not fallback
+      webpackConfig.resolve.symlinks = true;
 
       webpackConfig.plugins = [
         ...(webpackConfig.plugins || []),
-        new NodePolyfillPlugin()
+        new NodePolyfillPlugin(),
       ];
 
       return webpackConfig;
