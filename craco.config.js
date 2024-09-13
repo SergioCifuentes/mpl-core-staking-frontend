c9daca1c46c92e2e73dc2d723817631db80406ca
SergioCifuentes <sergiocifuentes1485@gmail.com>
2024-09-12 22:35:44 -0600

    build fix

--- craco.config.js
+++ craco.config.js
@@ -4,10 +4,8 @@ const path = require('path');
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Ensure Webpack resolves modules correctly
       webpackConfig.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules'];
 
-      // Set fallbacks for Node.js core modules no longer polyfilled in Webpack 5
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
         crypto: require.resolve('crypto-browserify'),
@@ -16,10 +14,8 @@ module.exports = {
         process: require.resolve('process/browser'),
       };
 
-      // Set symlinks outside fallback
       webpackConfig.resolve.symlinks = true;
 
-      // Add Node polyfills for Webpack 5
       webpackConfig.plugins = [
         ...(webpackConfig.plugins || []),
         new NodePolyfillPlugin(),
@@ -28,13 +24,8 @@ module.exports = {
       return webpackConfig;
     },
   },
-
   babel: {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-
-    plugins: [
-      // Conditionally include 'react-refresh/babel' only in development mode
-      ...(process.env.NODE_ENV === 'development' ? [['react-refresh/babel', { skipEnvCheck: true }]] : []),
-    ],
+    plugins: [] // No plugins here to avoid conflicts
   },
 };
