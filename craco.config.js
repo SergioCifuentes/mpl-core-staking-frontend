cf9772e937628b36a36a8773cdeb32b197a18e07
SergioCifuentes <sergiocifuentes1485@gmail.com>
2024-09-12 22:10:36 -0600

    build fix

--- craco.config.js
+++ craco.config.js
@@ -1,31 +1,21 @@
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      // Ensure Webpack resolves modules correctly
-      webpackConfig.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules'];
+    configure: (webpackConfig, { env }) => {
+      if (env === 'development') {
+        webpackConfig.plugins.push(new ReactRefreshWebpackPlugin());
+      }
 
-      // Set fallbacks for Node.js core modules no longer polyfilled in Webpack 5
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        buffer: require.resolve('buffer/'),
-        process: require.resolve('process/browser'),
-      };
-
-      // Set symlinks outside fallback
-      webpackConfig.resolve.symlinks = true; // <-- Move symlinks out of fallback
-
-      // Add Node polyfills for Webpack 5
-      webpackConfig.plugins = [
-        ...(webpackConfig.plugins || []),
-        new NodePolyfillPlugin()
-      ];
+      webpackConfig.plugins.push(new NodePolyfillPlugin());
 
       return webpackConfig;
-    }
-  }
+    },
+  },
+  babel: {
+    plugins: [
+      process.env.NODE_ENV === 'development' && 'react-refresh/babel',
+    ].filter(Boolean),
+  },
 };
