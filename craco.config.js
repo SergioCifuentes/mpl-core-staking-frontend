a8d0f01b90fb2c7fbd9c34f5fd81a23383bb5110
SergioCifuentes <sergiocifuentes1485@gmail.com>
2024-09-12 22:28:25 -0600

    build fix

--- craco.config.js
+++ craco.config.js
@@ -2,10 +2,8 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  
   webpack: {
     configure: (webpackConfig) => {
-      
       // Ensure Webpack resolves modules correctly
       webpackConfig.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules'];
 
@@ -19,15 +17,24 @@ module.exports = {
       };
 
       // Set symlinks outside fallback
-      webpackConfig.resolve.symlinks = true; // <-- Move symlinks out of fallback
+      webpackConfig.resolve.symlinks = true;
 
       // Add Node polyfills for Webpack 5
       webpackConfig.plugins = [
         ...(webpackConfig.plugins || []),
-        new NodePolyfillPlugin()
+        new NodePolyfillPlugin(),
       ];
 
       return webpackConfig;
-    }
-  }
+    },
+  },
+
+  babel: {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+
+    plugins: [
+      // Conditionally include 'react-refresh/babel' only in development mode
+      ...(process.env.NODE_ENV === 'development' ? [['react-refresh/babel', { skipEnvCheck: true }]] : []),
+    ],
+  },
 };
